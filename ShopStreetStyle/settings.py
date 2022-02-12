80124f97246cab557f835f54d15be1e2126bc7b7
Ahmad N <84658803+Ahmad0222@users.noreply.github.com>
2022-02-12 09:49:57 +0500

    Update settings.py
    email security

--- ShopStreetStyle/settings.py
+++ ShopStreetStyle/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'Shopstreetstyle2@gmail.com'
-EMAIL_HOST_PASSWORD = "streetstyle744@gmail.com"
+EMAIL_HOST_PASSWORD = "****************"
 EMAIL_PORT = 587
